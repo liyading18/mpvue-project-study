829eb64357212863203b1c1b75ad786008d550f9
AngeviLee <568178364@qq.com>
2020-04-25 18:16:24 +0800

    微信小程序-webview介绍说明

---
+++ src/pages/webview/index.vue
@@ -0,0 +1,21 @@
+<template
+ <view class="page-body">
+   <!-- 个人类型不支持使用 -->
+  <view class="page-section page-section-gap">
+    <web-view src="https://github.com/AngeviLee/mpvue-project-study/commits/master"></web-view>
+  </view>
+</view>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+    .page-section-gap{
+        box-sizing: border-box;
+        padding: 0 30rpx;
+    }
+</style>
