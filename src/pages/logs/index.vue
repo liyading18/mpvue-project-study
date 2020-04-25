a12fc2a4b815b6af414f8dc7580a8ad2558710fa
AngeviLee <568178364@qq.com>
2020-04-25 17:25:56 +0800

    微信小程序-mpvue-单纯获取用户信息|拨打电话

--- src/pages/logs/index.vue
+++ src/pages/logs/index.vue
@@ -1,61 +1,49 @@
 <template>
-  <div>
-      <swiper v-if="imgUrls.length > 0" indidator-dots="imgUrls.length > 1" >
-      <block v-for="(item, index) in imgUrls" :key="index" >
-        <swiper-item>
-          <image :src="item" mode="scaleToFill"></image>
-        </swiper-item>
-      </block>
-    </swiper>
+    <div>
 
-    <ul class="container log-list">
-      <li v-for="(log, index) in logs" :class="{ red: aa }" :key="index" class="log-item">
-        <card :text="(index + 1) + ' . ' + log"></card>
-      </li>
-    </ul>
-  </div>
+      <!-- 如果只是单纯的获取用户头像，名称等 -->
+      <div class="avatar">
+        <open-data type='userAvatarUrl' class="avar-img"></open-data>
+        <open-data type='userNickName'></open-data>
+
+        <!-- 此处的联系客服是拨打电话 -->
+        <div style="padding-top: 20px;">
+          <span @click="handleCoact">联系客服</span>
+        </div>
+
+      </div>
+
+
+    </div>
 </template>
 
 <script>
-import { formatTime } from '@/utils/index'
-import card from '@/components/card'
 
 export default {
-  components: {
-    card
-  },
+  name: 'AppCenter',
 
-  data () {
-    return {
-      logs: [],
-      imgUrls: [
-        'http://mss.sankuai.com/v1/mss_51a7233366a4427fa6132a6ce72dbe54/newsPicture/05558951-de60-49fb-b674-dd906c8897a6',
-        'http://mss.sankuai.com/v1/mss_51a7233366a4427fa6132a6ce72dbe54/coursePicture/0fbcfdf7-0040-4692-8f84-78bb21f3395d',
-        'http://mss.sankuai.com/v1/mss_51a7233366a4427fa6132a6ce72dbe54/management-school-picture/7683b32e-4e44-4b2f-9c03-c21f34320870'
-      ]
+  methods: {
+    handleCoact() {
+      mpvue.makePhoneCall({
+        phoneNumber: '15931441062'
+      })
     }
-  },
-
-  created () {
-    let logs
-    if (mpvuePlatform === 'my') {
-      logs = mpvue.getStorageSync({key: 'logs'}).data || []
-    } else {
-      logs = mpvue.getStorageSync('logs') || []
-    }
-    this.logs = logs.map(log => formatTime(new Date(log)))
   }
 }
 </script>
 
-<style>
-.log-list {
-  display: flex;
-  flex-direction: column;
-  padding: 40rpx;
-}
-
-.log-item {
-  margin: 10rpx;
-}
+<style lang="less" scoped>
+  .avatar {
+    width: 200rpx;
+    height: 200rpx;
+    margin: 0 auto;
+    text-align: center;
+    .avar-img {
+       width: 200rpx;
+       height: 200rpx;
+      border-radius: 50%;
+      width: 100%;
+      height: 100%;
+    }
+  }
 </style>
